2459d3dbf60ad11468691fb8594462a146d7bbd4
diegoarmino <diegoarmino@gmail.com>
2015-07-13 09:36:11 -0400

    Fixed bug: hii was not declared in printrract

--- src/qvamod_lioexcl.f90
+++ src/qvamod_lioexcl.f90
@@ -1749,15 +1749,16 @@ contains
 
 !     PRINT RESONANT RAMAN ACTIVITY
 !     ------------------------------------------------------------------
-      call printrract(rract,nvdf)
+      call printrract(rract,nvdf,hii)
 
       end subroutine
 
-      subroutine printrract(rract,nvdf)
+      subroutine printrract(rract,nvdf,hii)
          implicit none
 !        --------------------------------------------------------------
          integer,intent(in)       :: nvdf
          real*8,intent(in)        :: rract(nvdf)
+         real*8,intent(in)        :: hii(nvdf)
          integer                  :: nm
 !        --------------------------------------------------------------
          
