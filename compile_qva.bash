e04ba0484ea9471b85997235c53cb637bf8e663b
diegoarmino <diegoarmino@gmail.com>
2016-04-11 14:27:02 -0400

    changed compile script for gator

--- compile_qva.bash
+++ compile_qva.bash
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 # Uncoment the following to load modules (where available).
-module load intel gsl cuda mkl
+#module load intel/2016.0.109 gsl cuda/7.0 
+module load intel/2013 gsl cuda/5.5
 
 # The following environment variables must be specified.
 export CUDA_HOME=$HPC_CUDA_DIR
-export MKL_HOME=/opt/intel/composerxe/mkl
-export LIO_HOME=${HOME}/progs/lio
+export CUDAHOME=$HPC_CUDA_DIR
+export MKL_HOME=$MKLROOT
+export LIO_HOME=${HOME}/dev/lio
+export LIOHOME=${HOME}/dev/lio
+export GARCHAHOME=${HOME}/dev/lio
 export QVA_HOME=${HOME}/dev/qumvia
+
+echo $CUDA_HOME
+echo $MKL_HOME
 
 cd ${QVA_HOME}/src
 
 # Uncomment to compile CPU version only
-#make cpu
+make cpu
 # Uncomment to compile the LIO-compatible (GPU) version only
-#make lio
+make lio
 # Uncomment to compile the LIO-compatible (GPU) library (for qumvia-amber) only
-make lib
+#make lib
