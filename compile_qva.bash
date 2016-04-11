9ac4124837acfd2c1183212f78bf86af987ef635
diegoarmino <diegoarmino@gmail.com>
2016-04-11 17:43:09 -0400

    trivial changes

--- compile_qva.bash
+++ compile_qva.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Uncoment the following to load modules (where available).
-module load intel gsl cuda mkl
+#module load intel/2016.0.109 gsl cuda mkl
 
 # The following environment variables must be specified.
-export CUDA_HOME=$HPC_CUDA_DIR
-export MKL_HOME=/opt/intel/composerxe/mkl
-export LIO_HOME=${HOME}/progs/lio
-export QVA_HOME=${HOME}/dev/qumvia
+#export CUDA_HOME=$HPC_CUDA_DIR
+#export MKL_HOME=$MKLROOT
+#export LIO_HOME=${HOME}/dev/lio
+#export QVA_HOME=${HOME}/dev/qumvia
 
 cd ${QVA_HOME}/src
 
 # Uncomment to compile CPU version only
-make cpu
+#make cpu
 # Uncomment to compile the LIO-compatible (GPU) version only
 make lio
